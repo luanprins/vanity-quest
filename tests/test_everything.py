8cac80824b3d62e57bc26da7ac0574b24be54360
Luan <luanprinsmain@gmail.com>
2021-09-01 19:51:55 +0200

    Added test_scene3 to test_everything.py

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -2,6 +2,7 @@ import unittest
 from .vanity_test_tools import *
 from .test_scene import *
 from .test_scene1 import *
+from .test_scene3 import *
 
 if __name__ == "__main__":
     unittest.main()
